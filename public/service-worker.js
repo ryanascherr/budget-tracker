ab559300a1204e721bd65cafed7ae96e02a8ac3f
ryanascherr <ryanascherr@gmail.com>
2021-07-06 11:05:09 -0400

    Added another file to cache in the service-worker file.

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,6 +8,7 @@ const FILES_TO_CACHE = [
     '/icons/icon-512x512.png',
     '/styles.css',
     'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+    'https://cdn.jsdelivr.net/npm/chart.js@2.8.0'
   ];
   
   const PRECACHE = 'precache-v1';
